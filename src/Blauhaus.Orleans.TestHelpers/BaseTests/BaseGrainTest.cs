4786b61a6a4f05f5abe6c7c6adf45b9c27b97c21
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-11-13 14:02:21 +0200

    no message

---
+++ src/Blauhaus.Orleans.TestHelpers/BaseTests/BaseGrainTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Blauhaus.TestHelpers.BaseTests;
+using Moq;
+using Orleans;
+using Orleans.TestKit;
+
+namespace Blauhaus.Orleans.TestHelpers.BaseTests
+{
+
+    public abstract class BaseGrainTest<TSut, TId> : BaseServiceTest<TSut> where TSut : Grain
+    {
+        protected TestKitSilo _testSilo;
+
+        protected TId GrainId;
+        
+        protected DateTime SetupTime { get; private set; }
+        protected DateTime RunTime { get; private set; }
+
+        
+        protected void AddSiloService<T>(T service) where T : class
+        {
+            _testSilo.ServiceProvider.AddService(service);
+        }
+        
+
+    }
+
+    public abstract class BaseStringGrainTest<TSut> : BaseGrainTest<TSut, string> where TSut : Grain, IGrainWithStringKey
+    {
+        
+        protected Mock<T> AddMockGrain<T>(string grainKey) where T : class, IGrainWithStringKey
+        {
+            return _testSilo.AddProbe<T>(grainKey);
+        }
+        
+        protected override TSut ConstructSut()
+        {
+            return _testSilo.CreateGrainAsync<TSut>(GrainId).GetAwaiter().GetResult();
+        }
+    }
+}

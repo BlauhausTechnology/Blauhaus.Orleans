4517a13c7f9531ffba4784264f51dbb9a27dee58
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-02-16 08:26:44 +0200

    no message

--- src/Blauhaus.Orleans.Abstractions/Handlers/IDtoLoader.cs
+++ src/Blauhaus.Orleans.Abstractions/Handlers/IDtoLoader.cs
@@ -4,13 +4,13 @@ using Blauhaus.Domain.Abstractions.Entities;
 
 namespace Blauhaus.Orleans.Abstractions.Handlers
 {
-    public interface IDtoLoader<TDto> where TDto : ISyncClientEntity
+    public interface IDtoLoader<TDto> where TDto : IClientEntity
     {
         Task<TDto> GetDtoAsync();
     }
 
     
-    public interface IDtosLoader<TDto> where TDto : ISyncClientEntity
+    public interface IDtosLoader<TDto> where TDto : IClientEntity
     {
         Task<List<TDto>> GetDtosAsync();
     }

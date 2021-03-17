b6192438391c99eec58a88f1dd2407064a61696b
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-03-17 15:57:37 +0200

    no message

--- src/Blauhaus.Orleans/Grains/BaseGrain.cs
+++ src/Blauhaus.Orleans/Grains/BaseGrain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Blauhaus.Orleans.Abstractions.Streams;
 using Orleans;
 using Orleans.Streams;
@@ -12,18 +13,40 @@ namespace Blauhaus.Orleans.Grains
         protected IGrain GetGrain(Type grainInterfaceType, Guid id) => GrainFactory.GetGrain(grainInterfaceType, id);
         protected IGrain GetGrain(Type grainInterfaceType, string id) => GrainFactory.GetGrain(grainInterfaceType, id);
 
-        protected IAsyncStream<T> GeTransientStream<T>(Guid id, string name)
+        protected IAsyncStream<T> GeTransientStream<T>(Guid streamId, string streamEventName)
         {
             var streamProvider = GetStreamProvider(StreamProvider.Transient);
-            return streamProvider.GetStream<T>(id, name);
+            return streamProvider.GetStream<T>(streamId, streamEventName);
         }
 
-        protected IAsyncStream<T> GetPersistentStream<T>(Guid id, string name)
+        protected IAsyncStream<T> GetPersistentStream<T>(Guid streamId, string streamEventName)
         {
             var streamProvider = GetStreamProvider(StreamProvider.Persistent);
-            return streamProvider.GetStream<T>(id, name);
+            return streamProvider.GetStream<T>(streamId, streamEventName);
         }
-        
-        
+
+        protected async Task AddOrResumeTransientSubscriptionAsync<T>(Guid streamId, string streamEventName, Func<T, Task> handler)
+        {
+            var stream = this.GeTransientStream<T>(streamId, streamEventName);
+            
+            var existingHandles = await stream.GetAllSubscriptionHandles();
+            if (existingHandles.Count == 0)
+            {
+                await stream.SubscribeAsync(async (t, token) =>
+                {
+                    await handler.Invoke(t);
+                });
+            }
+            else
+            {
+                foreach (var streamSubscriptionHandle in existingHandles)
+                {
+                    await streamSubscriptionHandle.ResumeAsync(async (t, token) =>
+                    { 
+                        await handler.Invoke(t);
+                    });
+                }
+            }
+        }
     }
 }

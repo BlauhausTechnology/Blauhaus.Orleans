a2b661af2aa2d2f46a22f95a72a6f9f50d90fdcd
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-06-26 13:39:07 +0200

    no message

---
+++ src/Blauhaus.Orleans/Grains/BaseResolverGrain.cs
@@ -0,0 +1,35 @@
+﻿using Blauhaus.Orleans.Abstractions.Grains;
+using System;
+using Blauhaus.Orleans.Abstractions.Resolver;
+using Orleans;
+
+namespace Blauhaus.Orleans.Grains
+{
+    public abstract class BaseResolverGrain<TGrainResolver> : Grain where TGrainResolver : IGrainResolver
+    {
+        private readonly TGrainResolver _grainResolver;
+
+        protected TGrain ResolveSingleton<TGrain>() where TGrain : IGrainSingleton
+        {
+            return _grainResolver.ResolveSingleton<TGrain>();
+        }
+        protected TGrain Resolve<TGrain>(Guid id) where TGrain : IGrainWithGuidKey
+        {
+            return _grainResolver.Resolve<TGrain>(id);
+        }
+        protected TGrain Resolve<TGrain>(string id) where TGrain : IGrainWithStringKey
+        {
+            return _grainResolver.Resolve<TGrain>(id);
+        }
+        protected TGrain Resolve<TGrain>(long id) where TGrain : IGrainWithIntegerKey
+        {
+            return _grainResolver.Resolve<TGrain>(id);
+        }
+
+        protected BaseResolverGrain(TGrainResolver grainResolver)
+        {
+            _grainResolver = grainResolver;
+            _grainResolver.Initialize(()=> GrainFactory);
+        }
+    }
+}

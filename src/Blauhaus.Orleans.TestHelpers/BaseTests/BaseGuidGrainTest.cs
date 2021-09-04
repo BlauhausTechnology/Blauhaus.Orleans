d7f3629a695b2441c60968575db0ced34a91c0d8
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-09-04 12:45:57 +0200

    no message

---
+++ src/Blauhaus.Orleans.TestHelpers/BaseTests/BaseGuidGrainTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Blauhaus.Orleans.Grains;
+using Orleans;
+
+namespace Blauhaus.Orleans.TestHelpers.BaseTests
+{
+    public class BaseGuidGrainTest<TSut> : BaseGrainTest<TSut, Guid> where TSut : Grain, IGrainWithGuidKey
+    {
+        public override void Setup()
+        {
+            base.Setup();
+
+            GrainId = Guid.NewGuid();
+        }
+
+        protected override TSut ConstructGrain()
+        {
+            return Silo.CreateGrainAsync<TSut>(GrainId).GetAwaiter().GetResult();
+        }
+
+    }
+}

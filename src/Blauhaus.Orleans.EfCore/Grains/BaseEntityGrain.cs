92daf7a1f3f5397cb9af98bdd5d220bdc6ecfb4a
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-10-25 17:56:28 +0200

    no message

--- src/Blauhaus.Orleans.EfCore/Grains/BaseEntityGrain.cs
+++ src/Blauhaus.Orleans.EfCore/Grains/BaseEntityGrain.cs
@@ -3,11 +3,18 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Blauhaus.Analytics.Abstractions.Service;
+using Blauhaus.Auth.Abstractions.Errors;
+using Blauhaus.Auth.Abstractions.Extensions;
+using Blauhaus.Auth.Abstractions.User;
+using Blauhaus.Domain.Abstractions.Commands;
 using Blauhaus.Domain.Abstractions.DtoHandlers;
 using Blauhaus.Domain.Abstractions.Entities;
+using Blauhaus.Domain.Abstractions.Errors;
+using Blauhaus.Domain.Server.Entities;
 using Blauhaus.Orleans.Abstractions.Handlers;
 using Blauhaus.Orleans.Abstractions.Resolver;
 using Blauhaus.Orleans.Resolver;
+using Blauhaus.Responses;
 using Blauhaus.Time.Abstractions;
 using Microsoft.EntityFrameworkCore;
 using Orleans;
@@ -15,9 +22,9 @@ using EntityState = Blauhaus.Domain.Abstractions.Entities.EntityState;
 
 namespace Blauhaus.Orleans.EfCore.Grains
 {
-    public abstract class BaseEntityGrain<TDbContext, TEntity, TDto, TGrainResolver> : BaseEntityGrain<TDbContext, TEntity, TDto, TGrainResolver, Guid>
+    public abstract class BaseEntityGrain<TDbContext, TEntity, TDto, TGrainResolver> : BaseEntityGrain<TDbContext, TEntity, TGrainResolver>
         where TDbContext : DbContext 
-        where TEntity : class, IServerEntity
+        where TEntity : BaseServerEntity, IDtoOwner<TDto>
         where TDto : IClientEntity<Guid>
         where TGrainResolver : IGrainResolver
     {
@@ -29,48 +36,76 @@ namespace Blauhaus.Orleans.EfCore.Grains
                 : base(dbContextFactory, analyticsService, timeService, grainResolver)
         {
         }
-    }
 
-    
-    //this is temporary to accomodate moonbase with Guid ids and string dto ids
-    public abstract class BaseEntityGrain<TDbContext, TEntity, TDto, TGrainResolver, TDtoId> : BaseEntityGrain<TDbContext, TEntity, TGrainResolver>, IDtoOwner<TDto>
-        where TDbContext : DbContext 
-        where TEntity : class, IServerEntity
-        where TDto : IClientEntity<TDtoId>
-        where TGrainResolver : IGrainResolver
-    {
         
-        protected BaseEntityGrain(
-            Func<TDbContext> dbContextFactory, 
-            IAnalyticsService analyticsService, 
-            ITimeService timeService,
-            TGrainResolver grainResolver) 
-                : base(dbContextFactory, analyticsService, timeService, grainResolver)
+        public async Task<Response> HandleAsync(ActivateCommand command, IAuthenticatedUser user)
         {
-        } 
-        
-        public Task<TDto> GetDtoAsync()
-        {
-            try
+            return await TryExecuteDbCommandAsync(command, async (db, now) =>
             {
-                if (Entity == null)
+                if (!user.IsAdminUser())
+                    return TraceError(AuthError.NotAuthorized);
+
+                var currentEntityState = LoadedEntity.EntityState;
+                if (currentEntityState is not (EntityState.Draft or EntityState.Archived))
                 {
-                    throw new InvalidOperationException($"Cannot GetDto because {typeof(TEntity).Name} with id {Id} does not exist");
+                    return TraceError(DomainErrors.InvalidEntityState(currentEntityState));
                 }
-            
-                return GetDtoAsync(Entity);
-            }
-            catch (Exception e)
-            {
-                AnalyticsService.LogException(this, e);
-                throw;
-            }
+
+                db.Attach(LoadedEntity);
+                LoadedEntity.Activate(now);
+                
+                return await HandleActivatedAsync(LoadedEntity);
+            });
         }
 
-        protected abstract Task<TDto> GetDtoAsync(TEntity entity);
+        protected virtual Task<Response> HandleActivatedAsync(TEntity loadedEntity) => Response.SuccessTask();
 
+        public async Task<Response> HandleAsync(ArchiveCommand command, IAuthenticatedUser user)
+        {
+            return await TryExecuteDbCommandAsync(command, async (db, now) =>
+            {
+                if (!user.IsAdminUser())
+                    return TraceError(AuthError.NotAuthorized);
+
+                var currentEntityState = LoadedEntity.EntityState;
+                if (currentEntityState is not EntityState.Active)
+                {
+                    return TraceError(DomainErrors.InvalidEntityState(currentEntityState));
+                }
+
+                db.Attach(LoadedEntity);
+                LoadedEntity.Archive(now);
+
+                return await HandleArchivedAsync(LoadedEntity);
+
+            });
+        }
+
+        protected virtual Task<Response> HandleArchivedAsync(TEntity entity) => Response.SuccessTask();
+
+        public async Task<Response> HandleAsync(DeleteCommand command, IAuthenticatedUser user)
+        {
+            return await TryExecuteDbCommandAsync(command, async (db, now) =>
+            {
+                if (!user.IsAdminUser()) 
+                    return TraceError(AuthError.NotAuthorized);
+
+                var currentEntityState = LoadedEntity.EntityState;
+                if (currentEntityState is not (EntityState.Draft or EntityState.Archived))
+                {
+                    return TraceError(DomainErrors.InvalidEntityState(currentEntityState));
+                }
+
+                db.Attach(LoadedEntity);
+                LoadedEntity.Delete(now);
+                
+                return await HandleDeletedAsync(LoadedEntity); 
+            });
+        }
+        protected virtual Task<Response> HandleDeletedAsync(TEntity entity) => Response.SuccessTask();
     }
-    
+
+     
     
     public abstract class BaseEntityGrain<TDbContext, TEntity, TGrainResolver> : BaseDbGrain<TDbContext, TGrainResolver>, IGrainWithGuidKey
         where TDbContext : DbContext 

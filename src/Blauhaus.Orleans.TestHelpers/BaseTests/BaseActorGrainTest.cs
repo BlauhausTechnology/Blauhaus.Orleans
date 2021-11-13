539f406dd60b082e65670494d9aed90bdf65c6b4
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-11-13 14:01:44 +0200

    no message

--- src/Blauhaus.Orleans.TestHelpers/BaseTests/BaseActorGrainTest.cs
+++ src/Blauhaus.Orleans.TestHelpers/BaseTests/BaseActorGrainTest.cs
@@ -8,10 +8,23 @@ using Blauhaus.Orleans.Abstractions.Resolver;
 using Blauhaus.Orleans.Grains;
 using Blauhaus.Orleans.TestHelpers.MockBuilders.Resolver;
 using Moq;
+using Blauhaus.Orleans.Resolver;
 
 namespace Blauhaus.Orleans.TestHelpers.BaseTests
 {
-    public class BaseActorGrainTest<TGrain, TGrainResolver, TGrainResolverMockBuilder , TModel, TModelBuilder, TActor, TActorMockBuilder, TDto> : BaseGuidGrainTest<TGrain> 
+    public class BaseActorGrainTest<TGrain, TModel, TModelBuilder, TActor, TActorMockBuilder, TDto, TDtoBuilder> : BaseActorGrainTest<TGrain, IGrainResolver, GrainResolverMockBuilder , TModel, TModelBuilder, TActor, TActorMockBuilder, TDto, TDtoBuilder>
+        where TGrain : BaseActorGrain<IGrainResolver, TActor, TModel, TDto>
+        where TActor : class, IDtoModelActor<TModel, TDto, Guid>
+        where TModel : class, IHasId<Guid>
+        where TActorMockBuilder : BaseMockBuilder<TActorMockBuilder, TActor>, new()
+        where TModelBuilder : class, IBuilder<TModelBuilder, TModel>, new()
+        where TDto : IHasId<Guid>
+        where TDtoBuilder : class, IBuilder<TDtoBuilder, TDto>, new()
+    {
+
+    }
+    public class BaseActorGrainTest<TGrain, TGrainResolver, TGrainResolverMockBuilder , TModel, TModelBuilder, TActor, TActorMockBuilder, TDto, TDtoBuilder> 
+        : BaseGuidGrainTest<TGrain> 
         where TGrain : BaseActorGrain<TGrainResolver, TActor, TModel, TDto>
         where TActor : class, IDtoModelActor<TModel, TDto, Guid>
         where TModel : class, IHasId<Guid>
@@ -20,22 +33,29 @@ namespace Blauhaus.Orleans.TestHelpers.BaseTests
         where TDto : IHasId<Guid>
         where TGrainResolver : class, IGrainResolver
         where TGrainResolverMockBuilder : BaseGrainResolverMockBuilder<TGrainResolverMockBuilder, TGrainResolver>, new()
+        where TDtoBuilder : class, IBuilder<TDtoBuilder, TDto>, new()
     {
 
         protected TActorMockBuilder MockActor = null!;
         protected TModelBuilder ModelBuilder = null!;
         protected TGrainResolverMockBuilder MockGrainResolver = null!;
+        protected TDtoBuilder DtoBuilder = null!;
 
         public override void Setup()
         {
             base.Setup();
 
             GrainId = Guid.NewGuid();
+
             ModelBuilder = new TModelBuilder();
             ModelBuilder.With(x => x.Id, GrainId);
 
+            DtoBuilder = new TDtoBuilder();
+            DtoBuilder.With(x => x.Id, GrainId);
+
             MockActor = new TActorMockBuilder();
             MockActor.Mock.Setup(x => x.GetModelAsync()).ReturnsAsync(() => ModelBuilder.Object);
+            MockActor.Mock.Setup(x => x.GetDtoAsync()).ReturnsAsync(() => DtoBuilder.Object);
             AddSiloService(MockActor.Object);
 
             MockGrainResolver = new TGrainResolverMockBuilder();
